785ca5fbf514ed039ede2ebac0fdee27b9acf006
RahulSinghklr <rahulshiksingh@gmail.com>
2025-03-07 22:08:34 +0530

    4.2

---
+++ backend/models/Order.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  userId: mongoose.Schema.Types.ObjectId,
+  products: [{ productId: mongoose.Schema.Types.ObjectId, quantity: Number }],
+  paymentStatus: String
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
