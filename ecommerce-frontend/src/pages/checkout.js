7a293cd524f2b1cc479ecbef9aeb7c1053009bed
RahulSinghklr <rahulshiksingh@gmail.com>
2025-03-07 23:16:59 +0530

    front3

---
+++ ecommerce-frontend/src/pages/checkout.js
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import axios from "axios";
+
+export default function Checkout() {
+  const cart = useSelector((state) => state.cart);
+
+  const handleCheckout = async () => {
+    try {
+      const { data } = await axios.post("http://localhost:5000/api/orders", {
+        items: cart,
+      });
+      alert("Order placed successfully!");
+    } catch (error) {
+      console.error("Checkout error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <button onClick={handleCheckout}>Place Order</button>
+    </div>
+  );
+}
